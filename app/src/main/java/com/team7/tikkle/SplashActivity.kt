6e912e7c81c1e3d9801f309885cc20155d86cd02
Jeong <yuu-jeong@naver.com>
2023-03-07 23:40:36 +0900

    Feat : SplashActivity and View

---
+++ app/src/main/java/com/team7/tikkle/SplashActivity.kt
@@ -0,0 +1,27 @@
+package com.team7.tikkle
+
+import android.content.Intent
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.Window
+import android.view.WindowInsets
+import android.view.WindowManager
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        // Splash Handler
+        val handler = Handler(Looper.getMainLooper())
+        handler.postDelayed(Runnable {
+            Intent(this, MainActivity::class.java).apply {
+                startActivity(this)
+                finish()
+            }
+        }, 2500)
+    }
+}
