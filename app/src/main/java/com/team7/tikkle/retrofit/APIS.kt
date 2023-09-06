e614277f4bf7a270fdafaceb9abc8a09dc6e250c
Jeong <yuu-jeong@naver.com>
2023-09-06 16:04:10 +0900

    Fix : 메모 삭제 로직 수정

--- app/src/main/java/com/team7/tikkle/retrofit/APIS.kt
+++ app/src/main/java/com/team7/tikkle/retrofit/APIS.kt
@@ -220,11 +220,11 @@ interface APIS {
         @Header("X-ACCESS-TOKEN") accessToken: String
     ): MyStickerResponse
 
-    //전체 공개 메모 보기(cheer)
-//    @GET("/posts/estimating")
-//    suspend fun getRating(
-//        @Header("Authorization") accessToken: String
-//    ): Response<List<RatingResponse>>
+//    전체 공개 메모 보기(cheer)
+    @GET("/memo")
+    suspend fun getCheer(
+        @Header("X-ACCESS-TOKEN") accessToken: String
+    ): Response<CheerResponse>
 
     // 메모 이미지 삭제
     @DELETE("/memo/{id}/image")
