ede83b8cb02808e3ce9373711e3beb900aa57266
Jeong <yuu-jeong@naver.com>
2023-09-15 00:12:36 +0900

    Feat : memo unwritten todo API 연동

---
+++ app/src/main/java/com/team7/tikkle/data/ResponseUnwrittenTodo.kt
@@ -0,0 +1,13 @@
+package com.team7.tikkle.data
+
+data class ResponseUnwrittenTodo(
+    val code: Int,
+    val isSuccess: Boolean,
+    val message: String,
+    val result: List<UnwrittenResult>
+)
+
+data class UnwrittenResult(
+    val id: Long,
+    val title: String
+)
